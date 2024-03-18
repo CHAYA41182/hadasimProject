9fbf88dc7e32e3e6c966c63da79ef81060244fa9
chaya41182 <chaya41182@gmail.com>
2024-03-19 00:20:57 +0200

    Add cors and mongoose dependencies, update server.js to basic server

--- project-1/server/server.js
+++ project-1/server/server.js
@@ -1,7 +1,37 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 7002
+const dotenv = require('dotenv').config()
+const express = require('express');
+const cors = require('cors')
+const connectDB = require('./config/connectDB');
+const corsOption = require('./config/corsOption');
+const { default: mongoose } = require('mongoose');
+
+const app = express();
+
+
+const PORT = process.env.PORT || 7001
 
 
 
-app.listen(PORT, ()=>console.log(`server running on ${PORT}`))
+connectDB()
+
+app.use(cors(corsOption))
+app.use(express.json())
+app.use(express.static("public"))
+
+app.get("/",(req,res)=>{
+    res.send("home page")
+})
+
+
+
+
+mongoose.connection.once('open',()=>{
+    console.log("connected to DB successfully")
+    app.listen(PORT,()=>{
+        console.log(`server is running on port ${PORT}`)
+    })
+})
+
+mongoose.connection.on('error',(err)=>{
+    console.log("error conection to DB ",err)
+})
