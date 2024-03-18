cd692bb29321c6ed58f497088172cc5ff2061b4f
chaya41182 <chaya41182@gmail.com>
2024-03-19 01:16:25 +0200

    add member table

---
+++ project-1/server/models/Member.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const memberSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    dateBirth: {
+        type: Date,
+        required: true
+    },
+    gender: {
+        type: String,
+        enum: ['Male', 'Female'],
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    
+});
+
+module.exports = mongoose.model('Member', memberSchema);
