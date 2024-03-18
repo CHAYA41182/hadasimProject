0fc2099dd5b334ab602759bddae70ae2cdabfe32
chaya41182 <chaya41182@gmail.com>
2024-03-18 23:37:38 +0200

    Remove unnecessary route handler

--- project-1/server/server.js
+++ project-1/server/server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 7002
 
-app.get('/',(req,res)=>{
-res.send("Hello world!!")
-})
+
+
 app.listen(PORT, ()=>console.log(`server running on ${PORT}`))
