7fc2e39228f67c61a427ceab4a842dba8a442300
chaya41182 <chaya41182@gmail.com>
2024-03-20 17:41:48 +0200

    Update homepage layout and navigation texts

--- project-1/client/src/Pages/HomePage.js
+++ project-1/client/src/Pages/HomePage.js
@@ -5,20 +5,20 @@ import './HomePage.css';
 const HomePage = () => {
   return (
     <div className="homepage-container">
-      <h1>Welcome to the Health Fund System</h1>
+      <h1>ברוכים הבאים למערכת קופ"ח</h1>
       <nav className="navigation-menu">
         <ul>
           <li>
             <Link to="/members" className="nav-link">
-              View Member List
+              רשימת חברי הקופ"ח
             </Link>
-            <p className="nav-description">Browse all health fund members</p>
+            <p className="nav-description">צפו ברשימת כל חברי הקופ"ח</p>
           </li>
           <li>
             <Link to="/add-member" className="nav-link">
-              Add New Member
+              הוספת חבר חדש
             </Link>
-            <p className="nav-description">Register a new member to the health fund</p>
+            <p className="nav-description">הוסיפו חבר חדש למערכת הקופ"ח</p>
           </li>
           {/* Add more navigation items as needed */}
         </ul>
