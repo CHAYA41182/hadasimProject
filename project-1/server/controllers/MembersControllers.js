539dff77cf4b521a4b6114de63f1649ca8153489
chaya41182 <chaya41182@gmail.com>
2024-03-20 14:43:38 +0200

    Refactor MembersControllers.js to use Member model add getAll, create, update, delete

--- project-1/server/controllers/MembersControllers.js
+++ project-1/server/controllers/MembersControllers.js
@@ -1,11 +1,96 @@
-const MemberTable = require('../models/MemberTable');
+const Member = require('../models/Member');
 
 const getMembers = async (req, res) => {
     try {
-        const members = await MemberTable.find().lean();
+        const members = await Member.find().lean();
         res.json(members);
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 }
 
+const getMember = async (req, res) => {
+    try {
+        const member = await Member.findById(req.params.id).lean();
+        if (member) {
+            res.json(member);
+        } else {
+            res.status(404).json({ message: 'Member not found' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+const createMember = async (req, res) => {
+    const { firstName, lastName, tz, email, mobilePhone, coronaDetails } = req.body;
+    const { vaccinations, positiveTestDate, recoveryDate } = coronaDetails;
+    vaccinations.map(vaccination => {
+        const { date, manufacturer } = vaccination;
+        return { date, manufacturer };
+    });
+    const member = new Member({
+        firstName,
+        lastName,
+        tz,
+        email,
+        mobilePhone,
+        coronaDetails: {
+            vaccinations,
+            positiveTestDate,
+            recoveryDate
+        }
+    });
+    try {
+        const newMember = await member.save();
+        res.status(201).json(newMember);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+}
+
+const updateMember = async (req, res) => {
+    const { firstName, lastName, tz, email, mobilePhone, coronaDetails } = req.body;
+    const { vaccinations, positiveTestDate, recoveryDate } = coronaDetails;
+    vaccinations.map(vaccination => {
+        const { date, manufacturer } = vaccination;
+        return { date, manufacturer };
+    });
+    try {
+        const member = await Member.findById(req.params.id);
+        if (member) {
+            member.firstName = firstName;
+            member.lastName = lastName;
+            member.tz = tz;
+            member.email = email;
+            member.mobilePhone = mobilePhone;
+            member.coronaDetails = {
+                vaccinations,
+                positiveTestDate,
+                recoveryDate
+            };
+            const updatedMember = await member.save();
+            res.json(updatedMember);
+        } else {
+            res.status(404).json({ message: 'Member not found' });
+        }
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+}
+
+const deleteMember = async (req, res) => {
+    try {
+        const member = await Member.findById(req.params.id);
+        if (member) {
+            await member.remove();
+            res.json({ message: 'Member removed' });
+        } else {
+            res.status(404).json({ message: 'Member not found' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+module.exports = { getMembers, getMember, createMember, updateMember, deleteMember };
