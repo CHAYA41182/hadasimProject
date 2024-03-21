9efeda92f74201cf4956706d9352f54ee53357df
chaya41182 <chaya41182@gmail.com>
2024-03-21 23:59:45 +0200

    Add public folder to .gitignore

--- project-1/client/src/Components/MemberCard/MemberCard.js
+++ project-1/client/src/Components/MemberCard/MemberCard.js
@@ -12,10 +12,15 @@ const MemberCard = ({ member, onDelete }) => {
 
     return (
         <div className="member-card">
-            <h2>{`${member.firstName} ${member.lastName}`}</h2>
-            <p>{`מספר ת.ז: ${member.tz}`}</p>
-            <p>{member.phone? `טלפון: ${member.phone}` : member.mobilePhone ? `טלפון נייד: ${member.mobilePhone}` : ''}</p>
-            <p>{`תאריך לידה: ${(new Date(member.dateBirth)).toLocaleDateString()}` }</p>
+            <div>
+                <h2>{`${member.firstName} ${member.lastName}`}</h2>
+                <p>{`מספר ת.ז: ${member.tz}`}</p>
+                <p>{member.phone ? `טלפון: ${member.phone}` : member.mobilePhone ? `טלפון נייד: ${member.mobilePhone}` : ''}</p>
+                <p>{`תאריך לידה: ${(new Date(member.dateBirth)).toLocaleDateString()}`}</p>
+            </div>
+
+            {member.imageURL && <img src={member.imageURL} alt="member" />}
+
             <div className="member-card__actions">
                 <button onClick={() => navigate(`/members/${member._id}`)}>צפייה</button>
                 <button onClick={() => navigate(`/members/${member._id}/edit`)}>עריכה</button>
